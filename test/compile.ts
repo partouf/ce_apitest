a96964cc4bdafc2d31ee77f05242b44cb4c7efca
Partouf <partouf@gmail.com>
2020-08-09 21:27:12 +0200

    initial version

---
+++ test/compile.ts
@@ -0,0 +1,114 @@
+
+import { expect } from 'chai';
+import 'mocha';
+import { ICompilerFilters, ICompilationResult } from '@partouf/compilerexplorer-api';
+import { TrippleCompilationResult, doCompilation, findOrCreateCompilationByName, getJsonCompiler, getConfig } from '../utils/utils';
+
+
+async function getDefaultSquareCompilation(): Promise<TrippleCompilationResult> {
+    const source =
+`int square(int num) {
+    return num * num;
+}
+`;
+
+    const filters = {
+        binary: false,
+        demangle: true,
+        directives: true,
+        intel: true,
+        labels: true,
+        commentOnly: true,
+        libraryCode: false,
+    } as ICompilerFilters
+    
+    return doCompilation(source, filters);
+}
+
+async function getDefaultSquareNodemangleCompilation(): Promise<TrippleCompilationResult> {
+    const source =
+`int square(int num) {
+    return num * num;
+}
+`;
+
+    const filters = {
+        binary: false,
+        demangle: false,
+        directives: true,
+        intel: true,
+        labels: true,
+        commentOnly: true,
+        libraryCode: false,
+    } as ICompilerFilters
+    
+    return doCompilation(source, filters);
+}
+
+function testCompilationSuccess(result: ICompilationResult) {
+    expect(result.code).equal(0);
+    expect(result.asm?.length).not.equal(0);
+    expect(result.stdout?.length).equal(0);
+    expect(result.stderr?.length).equal(0);
+}
+
+
+describe('Api initialization', () => {
+    it('Should have a compiler', async () => {
+        const config = getConfig();
+        const compiler = await getJsonCompiler();
+        expect(compiler.id).equal(config.defaultCompilerId);
+        expect(compiler.supportsExecution()).true;
+    });
+});
+
+describe('Square example', async () => {
+    it('Should compile succesfully', async () => {
+        const results = await findOrCreateCompilationByName('defaultsquare', getDefaultSquareCompilation);
+
+        testCompilationSuccess(results.jsonResult);
+        testCompilationSuccess(results.textResult);
+        if (results.formResult)
+            testCompilationSuccess(results.formResult);
+    });
+
+    it('Should have some valid asm', async () => {
+        const results = await findOrCreateCompilationByName('defaultsquare', getDefaultSquareCompilation);
+
+        expect(results.jsonResultAsm).includes('mul');
+        expect(results.jsonResultAsm).includes('ret');
+
+        expect(results.textResultAsm).includes('mul');
+        expect(results.textResultAsm).includes('ret');
+
+        if (results.formResult) {
+            expect(results.formResultAsm).includes('mul');
+            expect(results.formResultAsm).includes('ret');
+        }
+    });
+
+    it('Should demangle', async () => {
+        const results = await findOrCreateCompilationByName('defaultsquare', getDefaultSquareCompilation);
+
+        expect(results.jsonResultAsm).includes('square(int)');
+        expect(results.textResultAsm).includes('square(int)');
+        if (results.formResult) {
+            expect(results.formResultAsm).includes('square(int)');
+        }
+    });
+
+    it('Should not demangle', async () => {
+        const results = await findOrCreateCompilationByName('defaultsquarenodemangle', getDefaultSquareNodemangleCompilation);
+
+        testCompilationSuccess(results.jsonResult);
+        expect(results.jsonResultAsm).not.includes('square(int)');
+
+        testCompilationSuccess(results.textResult);
+        expect(results.textResultAsm).not.includes('square(int)');
+
+        if (results.formResult) {
+            testCompilationSuccess(results.jsonResult);
+            expect(results.formResultAsm).not.includes('square(int)');
+        }
+    });
+});
---
+++ utils/utils.ts
@@ -0,0 +1,139 @@
+import { Api, ICompilationResult, APIType, ICompiler, ICompilerFilters } from "@partouf/compilerexplorer-api";
+
+export class TestConfig {
+    public jsonApi?: Api;
+    public textApi?: Api;
+    public formApi?: Api;
+    public defaultCompilerId: string = 'g91';
+}
+
+export class TrippleCompilationResult {
+    public jsonResult: ICompilationResult;
+    public textResult: ICompilationResult;
+    public formResult?: ICompilationResult;
+
+    public jsonResultAsm?: string;
+    public textResultAsm?: string;
+    public formResultAsm?: string;
+
+    constructor(json: ICompilationResult, text: ICompilationResult, form?: ICompilationResult) {
+        this.jsonResult = json;
+        this.textResult = text;
+        this.formResult = form;
+
+        this.jsonResultAsm = this.jsonResult.asm?.map((item) => item.text).join('\n');
+        this.textResultAsm = this.textResult.asm?.map((item) => item.text).join('\n');
+        if (this.formResult)
+            this.formResultAsm = this.formResult.asm?.map((item) => item.text).join('\n');
+    }
+}
+
+export class CompilationPair {
+    public name: string;
+    public results: Promise<TrippleCompilationResult>;
+
+    constructor(name: string, results: Promise<TrippleCompilationResult>) {
+        this.name = name;
+        this.results = results;
+    }
+}
+
+const config = new TestConfig();
+const compilationsByName: Array<CompilationPair> = [];
+
+export function getConfig(): TestConfig {
+    return config;
+}
+
+function initApis() {
+    if (!process.env.CEURL) {
+        throw 'Should set CEURL environment variable to the website URL to test';
+    }
+
+    if (process.env.DEFCOMPILERID) {
+        config.defaultCompilerId = process.env.DEFCOMPILERID;
+    }
+
+    if (!config.jsonApi) {
+        config.jsonApi = new Api({
+            url: process.env.CEURL,
+            defaultLanguage: 'c++',
+            apiType: APIType.JSON,
+        });
+    }
+
+    if (!config.textApi) {
+        config.textApi = new Api({
+            url: process.env.CEURL,
+            defaultLanguage: 'c++',
+            apiType: APIType.Text,
+        });
+    }
+
+    if (!config.formApi) {
+        config.formApi = new Api({
+            url: process.env.CEURL,
+            defaultLanguage: 'c++',
+            apiType: APIType.Form,
+        });
+    }
+}
+
+export async function getJsonCompiler(): Promise<ICompiler> {
+    initApis();
+
+    if (!config.jsonApi) throw 'Something went wrong with the jsonApi';
+
+    return await config.jsonApi.compilers.findById(config.defaultCompilerId);
+}
+
+export async function getTextCompiler(): Promise<ICompiler> {
+    initApis();
+
+    if (!config.textApi) throw 'Something went wrong with the textApi';
+
+    return await config.textApi.compilers.findById(config.defaultCompilerId);
+}
+
+export async function getFormCompiler(): Promise<ICompiler> {
+    initApis();
+
+    if (!config.formApi) throw 'Something went wrong with the formApi';
+
+    return await config.formApi.compilers.findById(config.defaultCompilerId);
+}
+
+export async function doCompilation(source: string,  filters: ICompilerFilters): Promise<TrippleCompilationResult> {
+    initApis();
+
+    let formResult: any = undefined;
+
+    const jsonCompiler = await getJsonCompiler();
+    const textCompiler = await getTextCompiler();
+
+    if (config.formApi) {
+        const formCompiler = await getFormCompiler();
+        formResult = await formCompiler.compile(source, [], undefined, filters, undefined);
+    }
+
+    const result = new TrippleCompilationResult(
+        await jsonCompiler.compile(source, [], undefined, filters, undefined),
+        await textCompiler.compile(source, [], undefined, filters, undefined),
+        formResult
+    );
+
+    return result;
+}
+
+export function findOrCreateCompilationByName(name: string, compilationFunc: CallableFunction): Promise<TrippleCompilationResult> {
+    for (const p of compilationsByName) {
+        if (p.name === name) {
+            return p.results;
+        }
+    }
+
+    const pair = new CompilationPair(name, compilationFunc());
+    compilationsByName.push(pair);
+
+    return pair.results;
+}
